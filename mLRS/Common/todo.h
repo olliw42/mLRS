740f8039b4f7e6ba82d711afd8e6575999baade9
olliw42 <waldmanns@gmx.de>
2022-01-14 10:12:09 +0100

    add todo list

---
+++ mLRS/Common/todo.h
@@ -0,0 +1,23 @@
+//*******************************************************
+// Copyright (c) MLRS project
+// GPL3
+// https://www.gnu.org/licenses/gpl-3.0.de.html
+// OlliW @ www.olliw.eu
+//*******************************************************
+// TODO
+//*******************************************************
+/*
+
+
+- get a clean HAL concept
+
+- get a clear mbridge and in concept, currently quite ugly as uart.h conflicts
+
+- handle allowed and not possible parameter combinations properly
+
+
+*/
+
+
+
+
