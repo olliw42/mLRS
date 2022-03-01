24881fee9fb0c114b761cd72283be44363f08fc3
olliw42 <waldmanns@gmx.de>
2022-03-01 09:28:36 +0100

    mbridge if not used, ups

--- mLRS/CommonTx/mbridge_interface.h
+++ mLRS/CommonTx/mbridge_interface.h
@@ -347,18 +347,7 @@ tMBridgeLinkStats lstats = {0};
 
 #else
 
-class tMBridgeDummy
-{
-  public:
-    void Init(void) {}
-    void putc(char c) {}
-    bool available(void) { return false; }
-    char getc(void) { return '\0'; }
-    void flush(void) {}
-};
-
-tMBridgeDummy bridge;
-
+tSerialBase bridge;
 
 #endif // if (defined USE_MBRIDGE) && (defined DEVICE_HAS_JRPIN5)
 
