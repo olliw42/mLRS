cf4db25cf91e0f4b550d1018a2e4f19ec35e3969
olliw42 <waldmanns@gmx.de>
2022-04-17 23:13:33 +0200

    setup enable bind phrase

--- mLRS/Common/setup.h
+++ mLRS/Common/setup.h
@@ -431,7 +431,7 @@ uint8_t doEEPROMwrite;
     setup_configure_metadata();
 
 // TODO: we currently force BindPhrase to the compile defaults, we first want to have bind
-    strncpy_x(Setup.BindPhrase, BIND_PHRASE, 6); // 6 chars
+//    strncpy_x(Setup.BindPhrase, BIND_PHRASE, 6); // 6 chars
 
 #ifdef SETUP_FORCE_COMMON_CONF
 setup_default();
