cc9b1483af2bf0cddc7539ad969eced94b0a9ab2
olliw42 <waldmanns@gmx.de>
2022-10-04 03:27:18 +0200

    v bump

--- mLRS/Common/common_conf.h
+++ mLRS/Common/common_conf.h
@@ -11,8 +11,8 @@
 #pragma once
 
 
-#define VERSION             212 // leading zero makes it octal!
-#define VERSIONONLYSTR      "v0.2.12"
+#define VERSION             213 // leading zero makes it octal!
+#define VERSIONONLYSTR      "v0.2.13"
 #define SETUPLAYOUT         2   // this should be changed then Setup struct and/or serial changes
 
 
