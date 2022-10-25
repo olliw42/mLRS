39bda83cca376e98494fcdc6ea8c530975e708e8
olliw42 <waldmanns@gmx.de>
2022-10-25 04:08:43 +0200

    v bump, pre-release

--- mLRS/Common/common_conf.h
+++ mLRS/Common/common_conf.h
@@ -11,8 +11,8 @@
 #pragma once
 
 
-#define VERSION             302 // leading zero makes it octal!
-#define VERSIONONLYSTR      "v0.3.02"
+#define VERSION             303 // leading zero makes it octal!
+#define VERSIONONLYSTR      "v0.3.03"
 #define SETUPLAYOUT         2   // this should be changed then Setup struct and/or serial changes
 
 
