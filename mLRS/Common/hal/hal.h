5e6b533abbf7b56064e0f4fa6d9a7f1020126605
vrquaeler <62423645+vrquaeler@users.noreply.github.com>
2022-10-30 11:03:53 +0100

    R9 mx as tx 2nd try (#33)
    * add TX_R9MM board def
    
    * add TX_R9MX board def
    
    * add tx-R9MX HAL description
    
    * tx-R9MX-l433cb as Sik replacement
    
    * invert SER or COM logic in tx-R9MX-l433cb
    
    * clean up, no tx-R9MM
    
    * clean up rx-R9MX project file
    
    * tx-R9MX: add IN-port

--- mLRS/Common/hal/hal.h
+++ mLRS/Common/hal/hal.h
@@ -102,6 +102,9 @@ Note: Some "high-level" features are set for each device in the device_conf.h fi
 #include "rx-hal-R9MM-868-f103rb.h"
 #endif
 
+#ifdef TX_R9MX_868_L433CB
+#include "tx-hal-R9MX-868-l433cb.h"
+#endif
 #ifdef TX_R9M_868_F103C8
 #include "tx-hal-R9M-868-f103c8.h"
 #endif
