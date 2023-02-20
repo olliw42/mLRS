8ac7f3190904ec1c611478bb173e7717721a4a64
jlpoltrack <41841496+jlpoltrack@users.noreply.github.com>
2023-02-20 13:10:14 -0500

    rx,tx 915 FCC FHSS definition update

--- mLRS/Common/setup.h
+++ mLRS/Common/setup.h
@@ -386,8 +386,12 @@ void setup_configure(void)
         }
         break;
     case SETUP_FREQUENCY_BAND_915_MHZ_FCC:
-        Config.FhssNum = FHSS_NUM_BAND_915_MHZ_FCC;
-        break;
+        switch (Config.Mode) {
+        case MODE_31HZ: Config.FhssNum = FHSS_NUM_BAND_915_MHZ_FCC; break;
+        case MODE_19HZ: Config.FhssNum = FHSS_NUM_BAND_915_MHZ_FCC_19HZ_MODE; break;
+        default:
+            while (1) {} // must not happen, should have been resolved in setup_sanitize()
+        }
     case SETUP_FREQUENCY_BAND_868_MHZ:
         Config.FhssNum = FHSS_NUM_BAND_868_MHZ;
         break;
