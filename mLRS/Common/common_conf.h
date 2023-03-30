c84226b6df0220464c73457316b3f09329ff7a51
olliw42 <waldmanns@gmx.de>
2023-03-30 12:28:56 +0200

    v bump v0.3.20, prerelease

--- mLRS/Common/common_conf.h
+++ mLRS/Common/common_conf.h
@@ -11,8 +11,8 @@
 #pragma once
 
 
-#define VERSION             319 // leading zero makes it octal!
-#define VERSIONONLYSTR      "v0.3.19"
+#define VERSION             320 // leading zero makes it octal!
+#define VERSIONONLYSTR      "v0.3.20"
 #define SETUPLAYOUT         2   // this should be changed then Setup struct and/or serial changes
 
 
