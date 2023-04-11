890da512acfd69d98e25cf559015d583f2362754
brad112358 <brad112358@gmail.com>
2023-04-11 17:30:04 -0500

    Don't send so many very short UDP messages to avoid message loss
    * Don't send so many very short UDP messages to avoid message loss
    * Extend timout and maximum UDP payload and avoid static local variable.
    * Fix missed type correction

--- esp/mlrs-wifi-bridge/mlrs-wifi-bridge.ino
+++ esp/mlrs-wifi-bridge/mlrs-wifi-bridge.ino
@@ -110,7 +110,8 @@ int led_tlast_ms;
 bool led_state;
 
 bool is_connected;
-int is_connected_tlast_ms;
+unsigned long is_connected_tlast_ms;
+unsigned long send_tlast_ms;
 
 
 void serialFlushRx(void)
@@ -169,14 +170,14 @@ void setup()
 
     is_connected = false;
     is_connected_tlast_ms = 0;
-
+    send_tlast_ms = 0;
     serialFlushRx();
 }
 
 
 void loop() 
 {
-    int tnow_ms = millis();
+    unsigned long tnow_ms = millis();
     if (is_connected && (tnow_ms - is_connected_tlast_ms > 2000)) { // nothing from GCS for 2 secs
         is_connected = false;
     }
@@ -199,12 +200,14 @@ void loop()
         is_connected = true;
         is_connected_tlast_ms = millis();;
     }
-
-    while (SERIAL.available()) {
+    int avail = SERIAL.available();
+    // Don't send nearly empty messages so often
+    if ((avail > 90) || (avail && (tnow_ms - send_tlast_ms > 15))) {
         int len = SERIAL.read(buf, sizeof(buf));
         udp.beginPacket(ip_udp, port_udp);
         udp.write(buf, len);
-        udp.endPacket();    
+        udp.endPacket();
+        send_tlast_ms = tnow_ms;
     }   
 
 #else // TCP
