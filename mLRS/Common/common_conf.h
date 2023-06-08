c0f1dfbdc789b2c0678f75ccb5737775efc39ad7
olliw42 <waldmanns@gmx.de>
2023-06-08 08:14:52 +0200

    v bump v0.3.26, w/o firmware binaries To keep synchronity, the v bump practice needs to be changed: With any generation of binaries for version vXX the v needs to be bumped to vXX+1, so that the v of a binary can be precisely related to code.

--- mLRS/Common/common_conf.h
+++ mLRS/Common/common_conf.h
@@ -11,8 +11,8 @@
 #pragma once
 
 
-#define VERSION             325 // leading zero makes it octal!
-#define VERSIONONLYSTR      "v0.3.25"
+#define VERSION             326 // leading zero makes it octal!
+#define VERSIONONLYSTR      "v0.3.26"
 #define SETUPLAYOUT         2   // this should be changed then Setup struct and/or serial changes
 
 
