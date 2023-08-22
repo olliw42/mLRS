51d0e09c0ada49ca8962a6c597acf1670dde3bc9
Risto <rotorman@users.noreply.github.com>
2023-08-23 00:22:33 +0200

    wifi-bridge, add Bluetooth connectivity option, rename to wireless-bridge
    * Adds Bluetooth connectivity option to mLRS Bridge.
    Making mLRS bridge now mLRS WiFi & Bluetooth Bridge.
    
    Quick test with an Espressif ESP32-DevKitC V4 gave promising results. Would be sweet to get some further tests with other hw before merging.
    
    * Fix as requested by Olli.
    
    * Changed to PROTOCOL_TYPE, where 0 = WiFi TCP, 1 = WiFi UDP, 2 = Bluetooth.
    
    * Rename to "wireless".
    
    * Bugfix. idx can reach 256.
    
    * Changed to WIRELESS_PROTOCOL
    
    * Bluetooth device name -> "mLRS BT"
    
    * Variable name change to bluetooth_device_name as well.
    
    * Ad requested by Olli, changing to int.
    
    * Easier sectioning of user defined settings as suggested by Olli.
    
    * Olli prefers for loop to while, so here we go.
    
    * Implemented some suggestions by Olli.
    
    * Reverted version numbers
    
    * Bugfix thx to Olli!

--- esp/mlrs-wireless-bridge/mlrs-wireless-bridge.ino
+++ esp/mlrs-wireless-bridge/mlrs-wireless-bridge.ino
@@ -1,10 +1,10 @@
 //*******************************************************
-// mLRS WiFi Bridge
+// mLRS Wireless Bridge
 // Copyright (c) www.olliw.eu, OlliW, OlliW42
 // License: GPL v3
 // https://www.gnu.org/licenses/gpl-3.0.de.html
 //*******************************************************
-// Basic but effective & reliable transparent WiFi<->serial bridge
+// Basic but effective & reliable transparent WiFi and Bluetooth<->serial bridge
 //*******************************************************
 // 22. Aug. 2023
 //*********************************************************/
@@ -57,10 +57,11 @@ List of supported modules, and board which need to be selected
 // (for the generic module also SERIAL_RXD and SERIAL_TXD need to be defined)
 //#define USE_SERIAL_INVERTED
 
-// Wifi Protocol 0 = TCP, 1 = UDP
-#define WIFI_PROTOCOL  1
+#define WIRELESS_PROTOCOL 1 // 0 = WiFi TCP, 1 = WiFi UDP, 2 = Bluetooth
 
-// Wifi credentials
+
+//**********************//
+//*** WiFi settings ***//
 String ssid = "mLRS AP"; // Wifi name
 String password = ""; // "thisisgreat"; // WiFi password, "" makes it an open AP
 
@@ -68,9 +69,6 @@ IPAddress ip(192, 168, 4, 55); // connect to this IP // MissionPlanner default i
 
 int port_tcp = 5760; // connect to this port per TCP // MissionPlanner default is 5760
 int port_udp = 14550; // connect to this port per UDP // MissionPlanner default is 14550
-
-// baudrate
-int baudrate = 115200;
 
 // WiFi channel
 // 1 is the default, 13 (2461-2483 MHz) has the least overlap with mLRS 2.4 GHz frequencies.
@@ -82,6 +80,17 @@ int wifi_channel = 13;
 // Note: In order to find the possible options, right click on WIFI_POWER_19_5dBm and choose "Go To Definiton"
 #define WIFI_POWER  WIFI_POWER_2dBm // WIFI_POWER_MINUS_1dBm is the lowest possible, WIFI_POWER_19_5dBm is the max
 
+
+//**************************//
+//*** Bluetooth settings ***//
+String bluetooth_device_name = "mLRS BT"; // Bluetooth device name
+
+
+//************************//
+//*** General settings ***//
+
+// baudrate
+int baudrate = 115200;
 
 // serial port usage (only effective for the generic module)
 // comment all for default behavior, which is using only Serial port
@@ -98,28 +107,36 @@ int wifi_channel = 13;
 // Includes
 //-------------------------------------------------------
 
-#include <WiFi.h>
+#if ((WIRELESS_PROTOCOL == 0) || (WIRELESS_PROTOCOL == 1)) // WiFi
+  #include <WiFi.h>
+#elif (WIRELESS_PROTOCOL == 2) // Bluetooth
+  #include <BluetoothSerial.h>
+#endif
 
 
 //-------------------------------------------------------
 // Module details
 //-------------------------------------------------------
 
-#include "mlrs-wifi-bridge-boards.h"
+#include "mlrs-wireless-bridge-boards.h"
 
 
 //-------------------------------------------------------
 // Internals
 //-------------------------------------------------------
 
-IPAddress ip_udp(ip[0], ip[1], ip[2], ip[3]+1); // speculation: it seems that MissionPlanner wants it +1
-IPAddress netmask(255, 255, 255, 0);
-#if WIFI_PROTOCOL == 1 // UDP
-WiFiServer server(80);
-WiFiUDP udp;
-#else // TCP
-WiFiServer server(port_tcp);
-WiFiClient client;
+#if ((WIRELESS_PROTOCOL == 0) || (WIRELESS_PROTOCOL == 1)) // WiFi
+  IPAddress ip_udp(ip[0], ip[1], ip[2], ip[3]+1); // speculation: it seems that MissionPlanner wants it +1
+  IPAddress netmask(255, 255, 255, 0);
+  #if WIRELESS_PROTOCOL == 1 // UDP
+    WiFiServer server(80);
+    WiFiUDP udp;
+  #else // TCP
+    WiFiServer server(port_tcp);
+    WiFiClient client;
+  #endif
+#elif (WIRELESS_PROTOCOL == 2) // Bluetooth
+  BluetoothSerial SerialBT;
 #endif
 
 bool led_state;
@@ -161,14 +178,15 @@ void setup()
     DBG_PRINTLN(rxbufsize);
     DBG_PRINTLN(txbufsize);
 
+#if ((WIRELESS_PROTOCOL == 0) || (WIRELESS_PROTOCOL == 1)) // WiFi
     // AP mode
-    //WiFi.mode(WIFI_AP); // seems not to be needed, done by WiFi.softAP()?
+    // WiFi.mode(WIFI_AP); // seems not to be needed, done by WiFi.softAP()?
     WiFi.softAPConfig(ip, ip, netmask);
-#if WIFI_PROTOCOL == 1
+  #if (WIRELESS_PROTOCOL == 1)
     String ssid_full = ssid + " UDP";
-#else    
+  #else    
     String ssid_full = ssid + " TCP";
-#endif    
+  #endif    
     WiFi.softAP(ssid_full.c_str(), (password.length()) ? password.c_str() : NULL, wifi_channel); // channel = 1 is default
     DBG_PRINT("ap ip address: ");
     DBG_PRINTLN(WiFi.softAPIP()); // comes out as 192.168.4.1
@@ -178,12 +196,15 @@ void setup()
     server.begin();
     server.setNoDelay(true);
 
-#ifdef WIFI_POWER
+  #ifdef WIFI_POWER
     WiFi.setTxPower(WIFI_POWER); // set WiFi power, AP or STA must have been started, returns false if it fails
-#endif    
-#if WIFI_PROTOCOL == 1
+  #endif    
+  #if (WIRELESS_PROTOCOL == 1)
     udp.begin(port_udp);
-#endif    
+  #endif
+#elif (WIRELESS_PROTOCOL == 2) // Bluetooth
+    SerialBT.begin(bluetooth_device_name);
+#endif
 
     led_tlast_ms = 0;
     led_state = false;
@@ -211,36 +232,11 @@ void loop()
         if (led_state) led_on(is_connected); else led_off();
     }
 
-    //-- here comes the core code, handle wifi connection and do the bridge
+    //-- here comes the core code, handle WiFi or Bluetooth connection and do the bridge
 
     uint8_t buf[256]; // working buffer
 
-#if WIFI_PROTOCOL == 1 // UDP
-
-    int packetSize = udp.parsePacket();
-    if (packetSize) {
-        int len = udp.read(buf, sizeof(buf));
-        SERIAL.write(buf, len);
-        is_connected = true;
-        is_connected_tlast_ms = millis();
-    }
-
-    tnow_ms = millis(); // may not be relevant, but just update it
-    int avail = SERIAL.available();
-    if (avail <= 0) {
-        serial_data_received_tfirst_ms = tnow_ms;      
-    } else 
-    if ((tnow_ms - serial_data_received_tfirst_ms) > 10 || avail > 128) { // 10 ms at 57600 bps corresponds to 57 bytes, no chance for 128 bytes
-        serial_data_received_tfirst_ms = tnow_ms;
-
-        int len = SERIAL.read(buf, sizeof(buf));
-        udp.beginPacket(ip_udp, port_udp);
-        udp.write(buf, len);
-        udp.endPacket();
-    }
-
-#else // TCP
-
+#if (WIRELESS_PROTOCOL == 0) // WiFi TCP
     if (server.hasClient()) {
         if (!client.connected()) {
             client.stop(); // doesn't appear to make a difference
@@ -279,7 +275,49 @@ void loop()
         int len = SERIAL.read(buf, sizeof(buf));
         client.write(buf, len);
     }
+#elif (WIRELESS_PROTOCOL == 1) // WiFi UDP
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+        int len = udp.read(buf, sizeof(buf));
+        SERIAL.write(buf, len);
+        is_connected = true;
+        is_connected_tlast_ms = millis();
+    }
 
-#endif    
+    tnow_ms = millis(); // may not be relevant, but just update it
+    int avail = SERIAL.available();
+    if (avail <= 0) {
+        serial_data_received_tfirst_ms = tnow_ms;      
+    } else 
+    if ((tnow_ms - serial_data_received_tfirst_ms) > 10 || avail > 128) { // 10 ms at 57600 bps corresponds to 57 bytes, no chance for 128 bytes
+        serial_data_received_tfirst_ms = tnow_ms;
+
+        int len = SERIAL.read(buf, sizeof(buf));
+        udp.beginPacket(ip_udp, port_udp);
+        udp.write(buf, len);
+        udp.endPacket();
+    }
+#elif (WIRELESS_PROTOCOL == 2) // Bluetooth
+    int len = SerialBT.available();
+    if (len > 0) {
+        if (len > sizeof(buf)) len = sizeof(buf);
+        for (int i = 0; i < len; i++) buf[i] = SerialBT.read();
+        SERIAL.write(buf, len);
+        is_connected = true;
+        is_connected_tlast_ms = millis();
+    }
+    
+    tnow_ms = millis(); // may not be relevant, but just update it
+    int avail = SERIAL.available();
+    if (avail <= 0) {
+        serial_data_received_tfirst_ms = tnow_ms;      
+    } else {
+        if ((tnow_ms - serial_data_received_tfirst_ms) > 10 || avail > 128) { // 10 ms at 57600 bps corresponds to 57 bytes, no chance for 128 bytes
+            serial_data_received_tfirst_ms = tnow_ms;
+            int len = SERIAL.read(buf, sizeof(buf));
+            SerialBT.write(buf, len);
+        }	
+    }
+#endif  
 }
 
