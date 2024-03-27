eaa7002354b09e6ae91080c86b775302139d6188
Tom McAdam <mcadam1980@gmail.com>
2024-03-27 21:14:45 +0900

    Use built in ESP8266 timer rather than 3rd party library

--- mLRS/Common/hal/esp-rxclock.h
+++ mLRS/Common/hal/esp-rxclock.h
@@ -9,89 +9,44 @@
 #define ESP_RXCLOCK_H
 #pragma once
 
-
-#define USING_TIM_DIV1 true
-
-#if defined(ESP8266)
-  #include "ESP8266TimerInterrupt.h"
-  ESP8266Timer ITimer;
-#elif defined(ESP32) 
-  #include "ESP32TimerInterrupt.h"
-  ESP32Timer ITimer(1);
-#endif
-// we use a 10us time base, so that overrun is 655 ms
-// 65 ms would only be 3 packets
-// I have tested it with 1us time base, and it also works fine, but hey
-// Note that TIM2 may be 16 bit or 32 bit depending on which STM32 controller is used
-
-
 #define CLOCK_SHIFT_10US          100 // 75 // 100 // 1 ms
-
+#define CLOCK_CNT_1MS             100 // 10us interval 10us x 100 = 1000us        
 
 volatile bool doPostReceive;
 
 uint16_t CLOCK_PERIOD_10US; // does not change while isr is enabled, so no need for volatile
 
-//-------------------------------------------------------
-// Clock Handler - mimics some STM32 internals
-//-------------------------------------------------------
 
-class ClockHandler
-{
-  public:
-    void Init(void) {};
-    void Do(void);
-
-    uint32_t CNT = 0;
-    uint32_t CCR1 = 0;
-    uint32_t CCR3 = 0;
-    bool CC1_FLAG = true;
-    bool CC3_FLAG = true;
-};
-
-void ClockHandler::Do(void)
-{
-    CNT++;
-    if (CNT == CCR1){
-        CC1_FLAG = true;
-    }
-    if (CNT == CCR3){
-        CC3_FLAG = true;
-    }
-}
-
-ClockHandler espTIMER;
-
+volatile uint32_t CNT_10us = 0;
+volatile uint32_t CCR1 = 0;
+volatile uint32_t CCR3 = 0;
+volatile uint32_t MS_C = 0;
 
 //-------------------------------------------------------
 // Clock ISR
 //-------------------------------------------------------
 
 IRQHANDLER(
-#if defined(ESP8266)
-IRAM_ATTR void CLOCK_IRQHandler(void)
-#elif defined(ESP32)
-IRAM_ATTR bool CLOCK_IRQHandler(void * timerNo)
-#endif
+void IRAM_ATTR CLOCK_IRQHandler(void)
 {
-    espTIMER.Do();
+    CNT_10us++;
 
     // Call HAL_IncTick every 1ms
-    if ((espTIMER.CNT % 100) == 0) {HAL_IncTick();}
-    
-    if (espTIMER.CC1_FLAG) { // this is at about when RX was or was supposed to be received
-        espTIMER.CC1_FLAG = false;
-        espTIMER.CCR3 = espTIMER.CCR1 + CLOCK_SHIFT_10US; // next doPostReceive
-        espTIMER.CCR1 = espTIMER.CCR1 + CLOCK_PERIOD_10US; // next tick
-    }
-    if (espTIMER.CC3_FLAG) { // this is 1 ms after RX was or was supposed to be received
-        espTIMER.CC3_FLAG = false;
-        doPostReceive = true;
+    if (CNT_10us == MS_C) {
+        MS_C = CNT_10us + CLOCK_CNT_1MS; 
+        HAL_IncTick();
     }
 
-    #if defined(ESP32)
-    return true;
-    #endif
+    // this is at about when RX was or was supposed to be received
+    if (CNT_10us == CCR1) { 
+        CCR3 = CNT_10us + CLOCK_SHIFT_10US; // next doPostReceive
+        CCR1 = CNT_10us + CLOCK_PERIOD_10US; // next tick
+    }
+
+    // this is 1 ms after RX was or was supposed to be received
+    if (CNT_10us == CCR3) { 
+        doPostReceive = true;
+    }
 
 })
 
@@ -106,11 +61,7 @@ class RxClockBase
     void Init(uint16_t period_ms);
     void SetPeriod(uint16_t period_ms);
     void Reset(void);
-
-    void init_isr_off(void);
-    void enable_isr(void);
-
-    uint16_t tim_10us(void);
+    void disable_isr(void);
 };
 
 
@@ -119,47 +70,32 @@ void RxClockBase::Init(uint16_t period_ms)
     CLOCK_PERIOD_10US = period_ms * 100; // frame rate in units of 10us
     doPostReceive = false;
 
-    init_isr_off();
-    enable_isr();
+    // Initialise the timer
+    timer1_attachInterrupt(CLOCK_IRQHandler); 
+    timer1_enable(TIM_DIV16, TIM_EDGE, TIM_LOOP);
+    timer1_write(50); //5MHz (5 ticks/us - 1677721.4 us max), 50 ticks = 10us
+    Reset();
 }
 
+void RxClockBase::disable_isr(void)
+{
+    timer1_detachInterrupt(); 
+}
 
 void RxClockBase::SetPeriod(uint16_t period_ms)
 {
     CLOCK_PERIOD_10US = period_ms * 100;
 }
 
-
 void RxClockBase::Reset(void)
 {
     if (!CLOCK_PERIOD_10US) while (1) {}
 
     __disable_irq();
-    uint32_t CNT = espTIMER.CNT; // works for both 16 and 32 bit timer
-    espTIMER.CCR1 = CNT + CLOCK_PERIOD_10US;
-    espTIMER.CCR3 = CNT + CLOCK_SHIFT_10US;
-    espTIMER.CC1_FLAG = false; // important to do
-    espTIMER.CC3_FLAG = false;
+    CCR1 = CNT_10us + CLOCK_PERIOD_10US;
+    CCR3 = CNT_10us + CLOCK_SHIFT_10US;
+    MS_C = CNT_10us + CLOCK_CNT_1MS;
     __enable_irq();
-}
-
-
-void RxClockBase::init_isr_off(void)
-{
-    ITimer.attachInterruptInterval(10, CLOCK_IRQHandler);
-    ITimer.disableTimer();
-}
-
-
-void RxClockBase::enable_isr(void)
-{
-    ITimer.enableTimer();
-}
-
-
-uint16_t RxClockBase::tim_10us(void)
-{
-    return espTIMER.CNT; // return 16 bit even for 32 bit timer
 }
 
 #endif // ESP_RXCLOCK_H
