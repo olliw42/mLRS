3549b9278e11a5a14edd80b253f6e5238670212e
Tom McAdam <mcadam1980@gmail.com>
2024-03-29 00:13:23 +0900

    Disable interrupts to protect clock reset

--- mLRS/Common/hal/esp-rxclock.h
+++ mLRS/Common/hal/esp-rxclock.h
@@ -91,9 +91,11 @@ void RxClockBase::Reset(void)
 {
     if (!CLOCK_PERIOD_10US) while (1) {}
 
+    noInterrupts();
     CCR1 = CNT_10us + CLOCK_PERIOD_10US;
     CCR3 = CNT_10us + CLOCK_SHIFT_10US;
     MS_C = CNT_10us + CLOCK_CNT_1MS;
+    interrupts();
 }
 
 #endif // ESP_RXCLOCK_H
