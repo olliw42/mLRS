f589730e607b2df11f984b5d5748ff7a68ec9b0c
olliw42 <waldmanns@gmx.de>
2024-04-07 10:29:59 +0200

    esp-spi.h tidies and IRAM_ATTR

--- mLRS/Common/esp-lib/esp-spi.h
+++ mLRS/Common/esp-lib/esp-spi.h
@@ -31,128 +31,135 @@
 
 static inline void spi_select(void)
 {
-  SPI_SELECT_PRE_DELAY;
-  digitalWrite(SPI_CS_IO, LOW); // CS = low
-  SPI_SELECT_POST_DELAY;
+    SPI_SELECT_PRE_DELAY;
+    digitalWrite(SPI_CS_IO, LOW); // CS = low
+    SPI_SELECT_POST_DELAY;
 }
 
 
 static inline void spi_deselect(void)
 {
-  SPI_DESELECT_PRE_DELAY;
-  digitalWrite(SPI_CS_IO, HIGH); // CS = high
-  SPI_DESELECT_POST_DELAY;
+    SPI_DESELECT_PRE_DELAY;
+    digitalWrite(SPI_CS_IO, HIGH); // CS = high
+    SPI_DESELECT_POST_DELAY;
 }
+
+#endif // #ifdef SPI_CS_IO
+
 
 void spi_init(void)
 {
-  pinMode(SPI_CS_IO, OUTPUT);
+#ifdef SPI_CS_IO
+    pinMode(SPI_CS_IO, OUTPUT);
+#endif    
 
-  SPI.begin();
-  SPI.setFrequency(SPI_FREQUENCY);
-  SPI.setBitOrder(MSBFIRST);
-  SPI.setDataMode(SPI_MODE0);
+    SPI.begin();
+    SPI.setFrequency(SPI_FREQUENCY);
+    SPI.setBitOrder(MSBFIRST);
+    SPI.setDataMode(SPI_MODE0);
 }
 
 //-- transmit, transfer, read, write functions
 
 // to utilize ESP SPI Buffer
-static inline IRAM_ATTR void spi_transferbytes(uint8_t* dataout, uint8_t* datain, uint8_t len)
+IRAM_ATTR static inline void spi_transferbytes(uint8_t* dataout, uint8_t* datain, uint8_t len)
 {
-  SPI.transferBytes(dataout, datain, len);
+    SPI.transferBytes(dataout, datain, len);
 }
 
-// is blocking
-uint8_t spi_transmitchar(uint8_t c)
-{
-  return SPI.transfer(c);
-}
 
 // is blocking
-void spi_transfer(uint8_t* dataout, uint8_t* datain, uint16_t len)
+IRAM_ATTR uint8_t spi_transmitchar(uint8_t c)
 {
-  while (len) {
-    *datain = spi_transmitchar(*dataout);
-    dataout++;
-    datain++;
-    len--;
-  }
+    return SPI.transfer(c);
+}
+
+
+// is blocking
+IRAM_ATTR void spi_transfer(uint8_t* dataout, uint8_t* datain, uint16_t len)
+{
+    while (len) {
+        *datain = spi_transmitchar(*dataout);
+        dataout++;
+        datain++;
+        len--;
+    }
 }
 
 
 // sends a dummy char, returns the received byte
 // is blocking
-static inline uint8_t spi_readchar(void)
+IRAM_ATTR static inline uint8_t spi_readchar(void)
 {
-  return spi_transmitchar(0xFF);
+    return spi_transmitchar(0xFF);
 }
 
 
 // sends a char, ignores the received byte
 // is blocking
-static inline void spi_writechar(uint8_t c)
+IRAM_ATTR static inline void spi_writechar(uint8_t c)
 {
-  spi_transmitchar(c);
+    spi_transmitchar(c);
 }
+
 
 // sends a word, returns the received word
 // is blocking
-static inline uint16_t spi_transmitword(uint16_t w)
+IRAM_ATTR static inline uint16_t spi_transmitword(uint16_t w)
 {
-  return (((uint16_t)spi_transmitchar((uint8_t)(w >> 8))) << 8) + spi_transmitchar((uint8_t)(w));
+    return (((uint16_t)spi_transmitchar((uint8_t)(w >> 8))) << 8) + spi_transmitchar((uint8_t)(w));
 }
 
 
 // sends a dummy word, returns the received word
 // is blocking
-static inline uint16_t spi_readword(void)
+IRAM_ATTR static inline uint16_t spi_readword(void)
 {
-  return (((uint16_t)spi_transmitchar(0xFF)) << 8) + spi_transmitchar(0xFF);
+    return (((uint16_t)spi_transmitchar(0xFF)) << 8) + spi_transmitchar(0xFF);
 }
 
 
 // sends a word, ignores the received word
 // is blocking
-static inline void spi_writeword(uint16_t w)
+IRAM_ATTR static inline void spi_writeword(uint16_t w)
 {
-  spi_transmitchar((uint8_t)(w >> 8));
-  spi_transmitchar((uint8_t)(w));
+    spi_transmitchar((uint8_t)(w >> 8));
+    spi_transmitchar((uint8_t)(w));
 }
 
 
 // is blocking
-void spi_read(uint8_t* data, uint16_t len)
+IRAM_ATTR void spi_read(uint8_t* data, uint16_t len)
 {
-  while (len) {
-    *data = spi_readchar();
-    data++;
-    len--;
-  }
+    while (len) {
+        *data = spi_readchar();
+        data++;
+        len--;
+    }
 }
 
 
 // is blocking
-void spi_write(uint8_t* data, uint16_t len)
+IRAM_ATTR void spi_write(uint8_t* data, uint16_t len)
 {
-  while (len) {
-    spi_writechar(*data);
-    data++;
-    len--;
-  }
+    while (len) {
+        spi_writechar(*data);
+        data++;
+        len--;
+    }
 }
 
 
 // is blocking
-void spi_writecandread(uint8_t c, uint8_t* data, uint16_t datalen)
+IRAM_ATTR void spi_writecandread(uint8_t c, uint8_t* data, uint16_t datalen)
 {
-  spi_writechar(c);
-  while (datalen) {
-    *data = spi_readchar();
-    data++;
-    datalen--;
-  }
+    spi_writechar(c);
+    while (datalen) {
+        *data = spi_readchar();
+        data++;
+        datalen--;
+    }
 }
 
-#endif
 
 #endif // ESPLIB_SPI_H
