48e3d541c2b4b3dcd2a66074cdff0d4fa46cd83e
JLP <jlpoltrack@gmail.com>
2024-04-13 16:30:37 -0400

    slight cleanup

--- mLRS/Common/hal/esp/rx-hal-generic-900-esp8285.h
+++ mLRS/Common/hal/esp/rx-hal-generic-900-esp8285.h
@@ -57,10 +57,11 @@ IRAM_ATTR void sx_dio_enable_exti_isr(void)
     attachInterrupt(SX_DIO0, SX_DIO_EXTI_IRQHandler, RISING);
 }
 
-IRAM_ATTR void sx_dio_init_exti_isroff(void) 
+void sx_dio_init_exti_isroff(void) 
 {
     detachInterrupt(SX_DIO0);
 }
+
 void sx_dio_exti_isr_clearflag(void) {}
 
 
