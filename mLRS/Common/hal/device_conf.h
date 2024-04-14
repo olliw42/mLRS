34e3e3de4e2ee3ad03a9e7a0d8da6ca71ac76309
JLP <jlpoltrack@gmail.com>
2024-04-14 12:28:16 -0400

    rebase

--- mLRS/Common/hal/device_conf.h
+++ mLRS/Common/hal/device_conf.h
@@ -297,6 +297,8 @@ The default selection of frequency bands can be overruled by feature defines.
 // ESP Boards
 //-------------------------------------------------------
 
+//-- ELRS 868/915 MHz Generic Devices
+
 #ifdef RX_ELRS_GENERIC_900_ESP8285
   #define DEVICE_NAME "GENERIC 900"
   #define DEVICE_IS_RECEIVER
@@ -313,6 +315,15 @@ The default selection of frequency bands can be overruled by feature defines.
   #define FREQUENCY_BAND_915_MHZ_FCC
 #endif
 
+#ifdef RX_GENERIC_2400_PA_TD_ESP32
+  #define DEVICE_NAME "GENERIC 2400 PA"
+  #define DEVICE_IS_RECEIVER
+  #define DEVICE_HAS_SX128x
+  #define FREQUENCY_BAND_2P4_GHZ
+#endif
+
+//-- ELRS 2.4 GHz Generic Devices
+
 #ifdef RX_ELRS_GENERIC_2400_ESP8285
   #define DEVICE_NAME "GENERIC 2400"
   #define DEVICE_IS_RECEIVER
@@ -326,6 +337,8 @@ The default selection of frequency bands can be overruled by feature defines.
   #define DEVICE_HAS_SX128x
   #define FREQUENCY_BAND_2P4_GHZ
 #endif
+
+//-- ELRS selected Devices
 
 #ifdef RX_ELRS_BAYCK_NANO_PRO_900_ESP8285
   #define DEVICE_NAME "BAYCK NANO PRO 900"
@@ -342,19 +355,14 @@ The default selection of frequency bands can be overruled by feature defines.
   #define FREQUENCY_BAND_2P4_GHZ
 #endif
 
+// -- DIY
+
 #ifdef RX_DIYBOARD_900_ESP8266
   #define DEVICE_NAME "DIY BOARD 900"
   #define DEVICE_IS_RECEIVER
   #define DEVICE_HAS_SX127x
   #define FREQUENCY_BAND_868_MHZ
   #define FREQUENCY_BAND_915_MHZ_FCC
-#endif
-
-#ifdef RX_GENERIC_2400_PA_TD_ESP32
-  #define DEVICE_NAME "GENERIC 2400 PA"
-  #define DEVICE_IS_RECEIVER
-  #define DEVICE_HAS_SX128x
-  #define FREQUENCY_BAND_2P4_GHZ
 #endif
 
 #ifdef RX_DEV_900_ESP32
