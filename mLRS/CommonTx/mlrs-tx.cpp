cef2da5379ca8f90f14190d9e1cd00181b397a15
olliw42 <waldmanns@gmx.de>
2024-05-20 08:20:20 +0200

    tx, ups, more formatting

--- mLRS/CommonTx/mlrs-tx.cpp
+++ mLRS/CommonTx/mlrs-tx.cpp
@@ -93,7 +93,7 @@ tTxCli cli;
 
 
 //-------------------------------------------------------
-// Mavlink
+// MAVLink
 //-------------------------------------------------------
 
 #include "mavlink_interface_tx.h"
@@ -1112,7 +1112,7 @@ IF_IN(
     case CLI_TASK_CHANGE_CONFIG_ID: config_id.Change(cli.GetTaskValue()); break;
     }
 
-    //-- Handle RSP wifi bridge
+    //-- Handle ESP wifi bridge
 
     esp.Do();
     uint8_t esp_task = esp.Task();
