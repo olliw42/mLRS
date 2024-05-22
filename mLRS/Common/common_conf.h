df814b64967213e9d157dff53e3749b54ac98949
olliw42 <waldmanns@gmx.de>
2024-05-22 06:49:11 +0200

    v1.2.04 (pre-release)

--- mLRS/Common/common_conf.h
+++ mLRS/Common/common_conf.h
@@ -11,8 +11,8 @@
 #pragma once
 
 
-#define VERSION             10203 // leading zero makes it octal!
-#define VERSIONONLYSTR      "v1.2.03"
+#define VERSION             10204 // leading zero makes it octal!
+#define VERSIONONLYSTR      "v1.2.04"
 #define SETUPLAYOUT         335 // this should be changed then Setup struct and/or serial changes
 
 
