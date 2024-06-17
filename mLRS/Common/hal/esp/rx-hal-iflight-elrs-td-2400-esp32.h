9c82f9a9c3da35200f6b24255c9753eed005a5d4
olliw42 <6089567+olliw42@users.noreply.github.com>
2024-06-17 13:58:58 +0200

    esp, rx, iflight2400 250mW, adjust power levels (#198)

--- mLRS/Common/hal/esp/rx-hal-iflight-elrs-td-2400-esp32.h
+++ mLRS/Common/hal/esp/rx-hal-iflight-elrs-td-2400-esp32.h
@@ -9,20 +9,23 @@
 //-------------------------------------------------------
 // ESP32, ELRS iFlight 2400 True Diversity PA RX
 //-------------------------------------------------------
+// antenna1 = left ufl
+// antenna2 = right ufl
 
 #include "rx-hal-generic-2400-td-pa-esp32.h"
 
 
 //-- POWER
 
-#define POWER_GAIN_DBM            27 // gain of a PA stage if present
-#define POWER_SX1280_MAX_DBM      SX1280_POWER_0_DBM  // maximum allowed sx power
+#define POWER_GAIN_DBM            25 // gain of a PA stage if present
+#define POWER_SX1280_MAX_DBM      (SX1280_POWER_0_DBM-1) // maximum allowed sx power
 #define POWER_USE_DEFAULT_RFPOWER_CALC
 
 #define RFPOWER_DEFAULT           0 // index into rfpower_list array
 
 const rfpower_t rfpower_list[] = {
-    { .dbm = POWER_10_DBM, .mW = 10 },
-    { .dbm = POWER_20_DBM, .mW = 100 },
-    { .dbm = POWER_24_DBM, .mW = 250 },
+    { .dbm = POWER_10_DBM, .mW = 10 }, // PA=27 ~8.2 dBm, 7 mW, PA=25 ~10.5 dBm, 11 mW
+    { .dbm = POWER_17_DBM, .mW = 50 }, //                        PA=25 ~17.0 dBm, 50 mW
+    { .dbm = POWER_20_DBM, .mW = 100 }, // PA=27 ~18.4 dBm, 70 mW, PA=25 ~20.2 dBm, 105 mW
+    { .dbm = POWER_24_DBM, .mW = 250 }, // PA=27 ~22.7 dBm, 190 mW, PA=25 ~24.6 dBm, 290 mW
 };
