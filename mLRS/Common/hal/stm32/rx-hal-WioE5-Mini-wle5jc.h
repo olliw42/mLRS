c2d08dc9c3c91bf561c3010d7b1d7168d00e602c
JLP <jlpoltrack@gmail.com>
2024-10-19 08:49:25 -0400

    no delay

--- mLRS/Common/hal/stm32/rx-hal-WioE5-Mini-wle5jc.h
+++ mLRS/Common/hal/stm32/rx-hal-WioE5-Mini-wle5jc.h
@@ -199,7 +199,6 @@ extern "C" { void delay_ms(uint16_t ms); }
 void systembootloader_init(void)
 {
     gpio_init(BOOT_BUTTON, IO_MODE_INPUT_PU, IO_SPEED_DEFAULT);
-    delay_ms(10);
     uint8_t cnt = 0;
     for (uint8_t i = 0; i < 16; i++) {
         if (gpio_read_activelow(BOOT_BUTTON)) cnt++;
