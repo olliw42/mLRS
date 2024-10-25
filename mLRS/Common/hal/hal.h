e611eedae9877227e0465c8876906c6940e7a428
olliw42 <waldmanns@gmx.de>
2024-10-25 05:38:49 +0200

    rx matek mr900-td g474ce

--- mLRS/Common/hal/hal.h
+++ mLRS/Common/hal/hal.h
@@ -92,9 +92,14 @@ Note: Some "high-level" features are set for each device in the device_conf.h fi
 
 //-- MATEKSYS mLRS devices
 
+#ifdef RX_MATEK_MR900_TD30_G474CE
+#include "matek/rx-hal-matek-mr900-td30-g474ce.h"
+#endif
+
 #ifdef TX_MATEK_MTX_DB30_G474CE
 #include "matek/tx-hal-matek-mtx-db30-g474ce.h"
 #endif
+
 
 #ifdef RX_MATEK_MR24_30_G431KB
 #include "matek/rx-hal-matek-mr24-30-g431kb.h"
---
+++ mLRS/Common/hal/matek/rx-hal-matek-mr900-td30-g474ce.h
@@ -0,0 +1,109 @@
+//*******************************************************
+// Copyright (c) MLRS project
+// GPL3
+// https://www.gnu.org/licenses/gpl-3.0.de.html
+//*******************************************************
+// hal
+//*******************************************************
+
+// MLRS_FEATURE defines usually must be defined very high up,
+// the following can however be used/defined locally here
+
+#define MLRS_FEATURE_CAN
+
+
+//-------------------------------------------------------
+// MATEKSYS mR900-30 STM32G431KB, as receiver
+//-------------------------------------------------------
+
+#define DEVICE_HAS_OUT
+#define DEVICE_HAS_FAN_ONOFF
+
+#ifdef MLRS_FEATURE_CAN
+#define DEVICE_HAS_DRONECAN
+#endif
+
+#include "hal-matek-mr-g431kb-common.h"
+
+
+//-- Timers, Timing, EEPROM, and such stuff
+
+#define CLOCK_TIMx                TIM2
+#define CLOCK_IRQn                TIM2_IRQn
+#define CLOCK_IRQHandler          TIM2_IRQHandler
+//#define CLOCK_IRQ_PRIORITY        10
+
+
+//-- UARTS
+// UARTB = serial port
+// UART = output port, SBus or whatever
+// UARTF = debug port
+
+#define UARTB_USE_UART1_PA9PA10 // serial
+#define UARTB_BAUD                RX_SERIAL_BAUDRATE
+#define UARTB_USE_TX
+#define UARTB_TXBUFSIZE           RX_SERIAL_TXBUFSIZE // 1024 // 512
+#define UARTB_USE_TX_ISR
+#define UARTB_USE_RX
+#define UARTB_RXBUFSIZE           RX_SERIAL_RXBUFSIZE // 1024 // 512
+
+#define UART_USE_UART2_PB3PB4 // out pin
+#define UART_BAUD                 100000 // SBus normal baud rate, is being set later anyhow
+#define UART_USE_TX
+#define UART_TXBUFSIZE            256 // 512
+#define UART_USE_TX_ISR
+//#define UART_USE_RX
+//#define UART_RXBUFSIZE            512
+
+#define UARTF_USE_LPUART1_PA2PA3 // debug
+#define UARTF_BAUD                115200
+#define UARTF_USE_TX
+#define UARTF_TXBUFSIZE           512
+#define UARTF_USE_TX_ISR
+//#define UARTF_USE_RX
+//#define UARTF_RXBUFSIZE           512
+
+
+#define CAN_USE_FDCAN2_PB5PB6
+
+
+//-- SX1: SX12xx & SPI
+
+
+//-- Out port
+// this is nasty, UART defines not yet known, but cumbersome to add, so we include the lib
+#include "../../../modules/stm32ll-lib/src/stdstm32-uart.h"
+
+void out_init_gpio(void)
+{
+}
+
+void out_set_normal(void)
+{
+    LL_USART_Disable(UART_UARTx);
+    LL_USART_SetTXPinLevel(UART_UARTx, LL_USART_TXPIN_LEVEL_STANDARD);
+    LL_USART_Enable(UART_UARTx);
+}
+
+void out_set_inverted(void)
+{
+    LL_USART_Disable(UART_UARTx);
+    LL_USART_SetTXPinLevel(UART_UARTx, LL_USART_TXPIN_LEVEL_INVERTED);
+    LL_USART_Enable(UART_UARTx);
+}
+
+
+//-- Button
+
+
+//-- LEDs
+
+
+//-- Cooling Fan
+
+
+//-- POWER
+
+#define POWER_PA_MATEK_MR900_30
+#include "../hal-power-pa.h"
+
