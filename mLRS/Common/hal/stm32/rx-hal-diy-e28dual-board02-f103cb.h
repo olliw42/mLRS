ab3cb57e82541a1e3bba1244662c5a1a4226fd5f
JLP <jlpoltrack@gmail.com>
2024-11-27 21:29:16 -0500

    f1 e28 spi dc-dc

--- mLRS/Common/hal/stm32/rx-hal-diy-e28dual-board02-f103cb.h
+++ mLRS/Common/hal/stm32/rx-hal-diy-e28dual-board02-f103cb.h
@@ -75,7 +75,8 @@
 #define SPI_USE_SPI1 // PA5, PA6, PA7
 #define SPI_CS_IO                 IO_PB0
 #define SPI_USE_CLK_LOW_1EDGE     // datasheet says CPHA = 0  CPOL = 0
-#define SPI_USE_CLOCKSPEED_9MHZ
+#define SPI_USE_CLOCKSPEED_18MHZ
+#define SX_USE_REGULATOR_MODE_DCDC
 
 #define SX_RESET                  IO_PB5
 #define SX_DIO1                   IO_PB3
@@ -147,7 +148,8 @@ void sx_dio_exti_isr_clearflag(void)
 #define SPIB_USE_SPI2             // PB13, PB14, PB15
 #define SPIB_CS_IO                IO_PA8
 #define SPIB_USE_CLK_LOW_1EDGE    // datasheet says CPHA = 0  CPOL = 0
-#define SPIB_USE_CLOCKSPEED_9MHZ
+#define SPIB_USE_CLOCKSPEED_18MHZ
+#define SX2_USE_REGULATOR_MODE_DCDC
 
 #define SX2_RESET                 IO_PA4
 #define SX2_DIO1                  IO_PA1
