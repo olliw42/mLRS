1b0000fa55d6de4b7e007634f3698451026ce92b
JLP <jlpoltrack@gmail.com>
2025-01-25 11:02:38 -0500

    comment update

--- mLRS/Common/hal/esp/rx-hal-radiomaster-xr1-900-esp32c3.h
+++ mLRS/Common/hal/esp/rx-hal-radiomaster-xr1-900-esp32c3.h
@@ -34,7 +34,7 @@ void out_init_gpio(void) {}
 
 void out_set_normal(void)
 {
-    // https://github.com/espressif/esp-idf/blob/release/v4.4/components/esp_rom/include/esp32/rom/gpio.h#L228-L242
+    // https://github.com/espressif/esp-idf/blob/release/v4.4/components/esp_rom/include/esp32c3/rom/gpio.h#L236
     gpio_matrix_out((gpio_num_t)UART_USE_TX_IO, U1TXD_OUT_IDX, false, false);
 }
 
