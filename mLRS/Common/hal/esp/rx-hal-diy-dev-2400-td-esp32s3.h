a3f2454fcde9971e370472c030d872c494c8ad0a
JLP <jlpoltrack@gmail.com>
2025-04-04 12:16:02 -0400

    tidy

--- mLRS/Common/hal/esp/rx-hal-diy-dev-2400-td-esp32s3.h
+++ mLRS/Common/hal/esp/rx-hal-diy-dev-2400-td-esp32s3.h
@@ -21,23 +21,23 @@
 // UARTF = debug port
 
 #define UARTB_USE_SERIAL
+#define UARTB_BAUD                RX_SERIAL_BAUDRATE
 #define UARTB_USE_TX_IO           IO_P43
 #define UARTB_USE_RX_IO           IO_P44
-#define UARTB_BAUD                RX_SERIAL_BAUDRATE
 #define UARTB_TXBUFSIZE           RX_SERIAL_TXBUFSIZE
 #define UARTB_RXBUFSIZE           RX_SERIAL_RXBUFSIZE
 
 #define UART_USE_SERIAL1 
 #define UART_BAUD                 416666   // CRSF baud rate
-#define UART_USE_TX_IO            IO_P35   // TX2
-#define UART_USE_RX_IO            -1       // no Rx pin needed
+#define UART_USE_TX_IO            IO_P35   // TX2 pad
+#define UART_USE_RX_IO            -1       
 #define UART_TXBUFSIZE            256
 
 #define UARTF_USE_SERIAL2
 #define UARTF_BAUD                115200
-#define UARTF_USE_TX_IO           IO_P37   // TX3
-#define UARTF_USE_RX_IO           -1
-#define UARTF_TXBUFSIZE           0  // TX FIFO = 128
+#define UARTF_USE_TX_IO           IO_P37   // TX3 pad
+#define UARTF_USE_RX_IO           -1       // no Rx pin needed
+#define UARTF_TXBUFSIZE           0        // TX FIFO = 128
 
 
 //-- SX1: SX128x & SPI
