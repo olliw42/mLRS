62cd4216701f68ea43c8f2a9f3bc7ac99be0a7e0
JLP <jlpoltrack@gmail.com>
2025-11-22 18:57:11 -0500

    cleaner, subtle bug on passthroughj

--- mLRS/Common/esp-lib/esp-rgb-led.h
+++ mLRS/Common/esp-lib/esp-rgb-led.h
@@ -10,82 +10,38 @@
 
 #include <NeoPixelBus.h>
 
-bool ledRedState;
-bool ledGreenState;
-bool ledBlueState;
+// Constants for the state manager
+#define LED_OFF 0
+#define LED_RED 1
+#define LED_GREEN 2
+#define LED_BLUE 3
+
+uint8_t ledCurrentColorState = LED_OFF;
 
 NeoPixelBus<NeoGrbFeature, NeoEsp32Rmt0Ws2812xMethod> ledRGB(PIXEL_NUM, LED_RGB);
 
-void leds_init(void)
-{
-    ledRGB.Begin();
+void leds_init(void) { ledRGB.Begin(); }
+
+// Helper, ClearTo() overwrites the previous color, this function is mutually exclusive by design.
+IRAM_ATTR void set_led_color_and_state(uint8_t targetState, RgbColor color) {
+    if (ledCurrentColorState == targetState) return;
+    
+    ledRGB.ClearTo(color);
     ledRGB.Show();
+    ledCurrentColorState = targetState;
 }
 
-IRAM_ATTR void set_led_color(RgbColor color)
-{
-    for (uint8_t i = 0; i < PIXEL_NUM; i++)
-    {
-        ledRGB.SetPixelColor(i, color);
-    }
-    ledRGB.Show();
-}
+// LED Functions
+IRAM_ATTR void led_red_off(void) { set_led_color_and_state(LED_OFF, RgbColor(0)); } 
+IRAM_ATTR void led_red_on(void) { set_led_color_and_state(LED_RED, RgbColor(255, 0, 0)); }
+IRAM_ATTR void led_red_toggle(void) { (ledCurrentColorState == LED_RED) ? led_red_off() : led_red_on(); }
 
-IRAM_ATTR void led_red_off(void)
-{
-    if (!ledRedState) return;
-    set_led_color(RgbColor(0, 0, 0));
-    ledRedState = 0;
-}
+IRAM_ATTR void led_green_off(void) { set_led_color_and_state(LED_OFF, RgbColor(0)); }
+IRAM_ATTR void led_green_on(void) { set_led_color_and_state(LED_GREEN, RgbColor(0, 255, 0)); }
+IRAM_ATTR void led_green_toggle(void) { (ledCurrentColorState == LED_GREEN) ? led_green_off() : led_green_on(); }
 
-IRAM_ATTR void led_red_on(void)
-{
-    if (ledRedState) return;
-    set_led_color(RgbColor(255, 0, 0));
-    ledRedState = 1;
-}
-
-IRAM_ATTR void led_red_toggle(void)
-{
-    if (ledRedState) { led_red_off(); } else { led_red_on(); }
-}
-
-IRAM_ATTR void led_green_off(void)
-{
-    if (!ledGreenState) return;
-    set_led_color(RgbColor(0, 0, 0));
-    ledGreenState = 0;
-}
-
-IRAM_ATTR void led_green_on(void)
-{
-    if (ledGreenState) return;
-    set_led_color(RgbColor(0, 255, 0));
-    ledGreenState = 1;
-}
-
-IRAM_ATTR void led_green_toggle(void)
-{
-    if (ledGreenState) { led_green_off(); } else { led_green_on(); }
-}
-
-IRAM_ATTR void led_blue_off(void)
-{
-    if (!ledBlueState) return;
-    set_led_color(RgbColor(0, 0, 0));
-    ledBlueState = 0;
-}
-
-IRAM_ATTR void led_blue_on(void)
-{
-    if (ledBlueState) return;
-    set_led_color(RgbColor(0, 0, 255));
-    ledBlueState = 1;
-}
-
-IRAM_ATTR void led_blue_toggle(void)
-{
-    if (ledBlueState) { led_blue_off(); } else { led_blue_on(); }
-}
+IRAM_ATTR void led_blue_off(void) { set_led_color_and_state(LED_OFF, RgbColor(0)); }
+IRAM_ATTR void led_blue_on(void) { set_led_color_and_state(LED_BLUE, RgbColor(0, 0, 255)); }
+IRAM_ATTR void led_blue_toggle(void) { (ledCurrentColorState == LED_BLUE) ? led_blue_off() : led_blue_on(); }
 
 #endif // ESPLIB_RGB_LED_H
--- mLRS/Common/leds.h
+++ mLRS/Common/leds.h
@@ -119,7 +119,7 @@ class tLEDs
     void TickPassthrough_ms(void)
     {
         DECc(blink, SYSTICK_DELAY_MS(100));
-#ifndef DEVICE_HAS_SINGLE_LED
+#if !defined DEVICE_HAS_SINGLE_LED && !defined DEVICE_HAS_SINGLE_LED_RGB
         if (!blink) { led_green_toggle(); led_red_toggle(); }
 #else
         if (!blink) { led_red_toggle(); }
