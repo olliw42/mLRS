201e7dcf036a5e186d22016122c218de7a7e9fbd
JLP <jlpoltrack@gmail.com>
2026-03-27 05:21:50 -0400

    Mask back

--- mLRS/Common/sx-drivers/lr20xx.cpp
+++ mLRS/Common/sx-drivers/lr20xx.cpp
@@ -188,7 +188,7 @@ void Lr20xxDriverBase::SetDioRfSwitchConfig(uint8_t Dio, uint8_t Config)
 uint8_t buf[2];
 
     buf[0] = Dio; // allowed values are 5 - 11
-    buf[1] = (Config);
+    buf[1] = (Config & 0x1F);
 
     WriteCommand(LR20XX_CMD_SET_DIO_RF_SWITCH_CONFIG, buf, 2);
 }
