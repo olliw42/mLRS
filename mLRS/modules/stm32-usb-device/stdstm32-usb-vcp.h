072264ca05b00e5885f3788b6edf3511d276bd9e
olliw42 <waldmanns@gmx.de>
2026-05-30 11:45:43 +0200

    modules, usb device lib, tidy (nfc)

--- mLRS/modules/stm32-usb-device/stdstm32-usb-vcp.h
+++ mLRS/modules/stm32-usb-device/stdstm32-usb-vcp.h
@@ -44,7 +44,7 @@ void usb_getbuf(uint8_t* const buf, uint16_t len);
 
 uint8_t usb_tx_full(void);
 void usb_putc(uint8_t c);
-void usb_puts(const char *s);
+void usb_puts(const char* s);
 void usb_putbuf(uint8_t* const buf, uint16_t len);
 
 void usb_rx_flush(void);
