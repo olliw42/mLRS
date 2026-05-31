210592cbecc4ace7031eeb7b8e3bb3528f0657f1
olliw42 <waldmanns@gmx.de>
2026-05-31 13:59:37 +0200

    common, hal, tidies (nfc)

--- mLRS/Common/hal/hal.h
+++ mLRS/Common/hal/hal.h
@@ -52,13 +52,15 @@ In tx-hal files:
 #define DEVICE_HAS_COM_ON_USB       // board has the Com port on native USB
 #define DEVICE_HAS_NO_DEBUG         // board has no Debug port
 #define DEVICE_HAS_DEBUG_SWUART     // implement Debug as software UART
+#define DEVICE_HAS_SERIAL2          // board has a Serial2 port
 #define DEVICE_HAS_I2C_DISPLAY          // board has a DISPLAY on I2C, and 5-way switch
 #define DEVICE_HAS_I2C_DISPLAY_ROT180   // board has a DISPLAY on I2C, rotated 180°, and 5-way switch
 #define DEVICE_HAS_FIVEWAY          // board has 5-way switch (without display)
+#define DEVICE_HAS_SINGLE_LED       // board has only one LED
+#define DEVICE_HAS_SINGLE_LED_RGB   // board has only one LED which is RGB WS2812, and thus can do more colors
+#define DEVICE_HAS_NO_LED           // board has no LEDs at all
 #define DEVICE_HAS_BUZZER           // board has a Buzzer
 #define DEVICE_HAS_FAN_ONOFF        // board has a Fan, which can be set on or off
-#define DEVICE_HAS_I2C_DAC          // board has a DAC for power control on I2C
-#define DEVICE_HAS_SERIAL2          // board has a Serial2 port
 #define DEVICE_HAS_ESP_WIFI_BRIDGE_ON_SERIAL  // board has ESP32 or ESP82xx with RESET,GPIO support, on Serial port
 #define DEVICE_HAS_ESP_WIFI_BRIDGE_ON_SERIAL2 // board has ESP32 or ESP82xx with RESET,GPIO support, on Serial2 port
 #define DEVICE_HAS_ESP_WIFI_BRIDGE_W_PASSTHRU_VIA_JRPIN5  // board has ESP32 or ESP82xx with its passthrough via JRPin5 port
@@ -68,9 +70,7 @@ In tx-hal files:
 #define DEVICE_HAS_HC04_MODULE_ON_SERIAL      // board has HC04 module on Serial port
 #define DEVICE_HAS_HC04_MODULE_ON_SERIAL2     // board has HC04 module on Serial2 port
 #define DEVICE_HAS_SYSTEMBOOT       // board has a means to invoke the system bootloader on startup
-#define DEVICE_HAS_SINGLE_LED       // board has only one LED
-#define DEVICE_HAS_SINGLE_LED_RGB   // board has only one LED which is RGB WS2812, and thus can do more colors
-#define DEVICE_HAS_NO_LED           // board has no LEDs at all
+#define DEVICE_HAS_I2C_DAC          // board has a DAC for power control on I2C
 
 In rx-hal files:
 
@@ -82,12 +82,12 @@ In rx-hal files:
 #define DEVICE_HAS_NO_SERIAL        // board has no Serial port
 #define DEVICE_HAS_NO_DEBUG         // board has no Debug port
 #define DEVICE_HAS_DEBUG_SWUART     // implement Debug as software UART
-#define DEVICE_HAS_I2C_DAC          // board has a DAC for power control on I2C
-#define DEVICE_HAS_SYSTEMBOOT       // board has a means to invoke the system bootloader on startup
+#define DEVICE_HAS_DRONECAN         // board has a DroneCAN port
 #define DEVICE_HAS_SINGLE_LED       // board has only one LED
 #define DEVICE_HAS_SINGLE_LED_RGB   // board has only one LED which is RGB WS2812
 #define DEVICE_HAS_FAN_ONOFF        // board has a Fan, which can be set on or off
-#define DEVICE_HAS_DRONECAN         // board has a DroneCAN port
+#define DEVICE_HAS_I2C_DAC          // board has a DAC for power control on I2C
+#define DEVICE_HAS_SYSTEMBOOT       // board has a means to invoke the system bootloader on startup
 
 Note: Some "high-level" features are set for each device in the device_conf.h file, and not in the device's hal file.
 */
