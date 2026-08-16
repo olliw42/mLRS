9af157252771a6a334622b35c55074e4a1bdb633
JLP <jlpoltrack@gmail.com>
2026-08-16 16:54:32 -0400

    Remove GCC Warning

--- tools/run_make_firmwares.py
+++ tools/run_make_firmwares.py
@@ -63,11 +63,6 @@ def findSTM32CubeIDEGnuTools(search_root):
     try:
         for dirpath in os.listdir(st_dir):
             if 'mcu.externaltools.gnu-tools-for-stm32' in dirpath and gnu_dir_os_name in dirpath:
-                # the numbers after the string 'gnu-tools-for-stm32' contains the gnutools ver number, like .11.3
-                gnuver = int(dirpath.split('gnu-tools-for-stm32',1)[1][1:3])
-                if gnuver >= 12:
-                    print("WARNING: gnu-tools ver >= 12 found but skipped")
-                    continue
                 # the string after the last . contains a datum plus some other number
                 ver = int(dirpath[dirpath.rindex('.')+1:])
                 if ver > ver_nr:
