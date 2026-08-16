92294acd000a46a362ddac45c3428b17da0ac49c
jlpoltrack <41841496+jlpoltrack@users.noreply.github.com>
2026-08-15 20:45:46 -0400

    tx, mbridge, sx_serial, fix hiding warning

--- mLRS/CommonTx/sx_serial_interface_tx.h
+++ mLRS/CommonTx/sx_serial_interface_tx.h
@@ -22,6 +22,7 @@ extern tTxMsp msp;
 class tTxSxSerial : public tSerialBase
 {
   public:
+    using tSerialBase::Init; // would be hidden by Init(tSerialBase*) otherwise
     void Init(tSerialBase* const _mbridge);
 
     bool available(void) override;
