7889f2542f2bc2eae918395e683a042c2cc4a938
olliw42 <waldmanns@gmx.de>
2026-08-27 09:08:31 +0200

    esp

--- mLRS/modules/esp-lib/esp-mcu.h
+++ mLRS/modules/esp-lib/esp-mcu.h
@@ -9,10 +9,33 @@
 #define ESPLIB_MCU_H
 
 
+//-------------------------------------------------------
+// ESP Device Information
+//-------------------------------------------------------
+
+#define ESP_MCU_UID_LEN  12 // must be same as that for STM32
+
+void mcu_uid(uint8_t uid[ESP_MCU_UID_LEN])
+{
+#ifndef ESP8266
+uint8_t mac[6];
+
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);    
+
+    memcpy(uid, mac, 6);
+    memcpy(uid + 6, mac, 6);
+#endif    
+}
+
+
+//-------------------------------------------------------
+// BootLoaderInit
+//-------------------------------------------------------
+
 void BootLoaderInit(void)
 {
     // Not sure what this one needs for the Arduino.
-    // Maybe a can be a hard reset using reset pin.
+    // Maybe can be a hard reset using reset pin.
 };
 
 
